84e176381f60aaf69774ff69860ada1a2eb8f577
acastrounis <acdevmail@gmail.com>
2014-07-24 12:55:13 -0500

    Added initial template files

---
+++ vendor/libraries/jasmineSpecTemplate.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var appRoot = process.cwd();
+
+describe('a suite', function() {
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function(){
+
+  });
+
+  it('should ', function() {
+  });
+
+  describe('a suite', function() {
+
+    beforeEach(function() {
+
+    });
+
+    afterEach(function(){
+
+    });
+
+    it('should ', function() {
+    });
+
+  });
+
+});
